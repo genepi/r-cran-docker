d6b0e63689ccadab37d86b768e14f50950b051e0
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2018-02-05 11:47:24 +0100

    update test script

--- myscript.R
+++ myscript.R
@@ -1,7 +1,18 @@
+message ("load some packages")
 library(RColorBrewer)
 library(knitr)
 library(markdown)
 library(rmarkdown)
 library(ggplot2)
 library(data.table)
+message ("done")
+
+message ("execute some calculations")
 1+1
+message ("done")
+
+message ("Write some lines to output.txt")
+fileConn<-file("output.txt")
+writeLines(c("Hello","World"), fileConn)
+close(fileConn)
+message ("done")
